8f1706059149a9ac1ef123fb56a70ec89bbcc887
MarteinnG <101052414+MarteinnG@users.noreply.github.com>
2022-04-10 17:56:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# magic8_ball_flutter
+ 
---
+++ android/app/src/main/kotlin/com/example/magic8_ball_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magic8_ball_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
